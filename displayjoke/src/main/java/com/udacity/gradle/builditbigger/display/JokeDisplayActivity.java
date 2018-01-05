d2c69a247463e5a944e652ff6270bd33e0eb7b44
Vinayraj Singh <Singh@Singhs-iMac.local>
2018-01-05 11:27:03 +0530

    Removed Floating action button from Joke display activity

--- displayjoke/src/main/java/com/udacity/gradle/builditbigger/display/JokeDisplayActivity.java
+++ displayjoke/src/main/java/com/udacity/gradle/builditbigger/display/JokeDisplayActivity.java
@@ -1,11 +1,8 @@
 package com.udacity.gradle.builditbigger.display;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class JokeDisplayActivity extends AppCompatActivity {
 
@@ -15,15 +12,6 @@ public class JokeDisplayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_joke_display);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }

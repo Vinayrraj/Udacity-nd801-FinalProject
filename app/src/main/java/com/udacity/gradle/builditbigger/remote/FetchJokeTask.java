e6480b6d010749ae8ad59196bc34f30f784259da
Vinayraj Singh <Singh@Singhs-iMac.local>
2018-01-06 11:30:22 +0530

    Endpoint task in progress

--- app/src/main/java/com/udacity/gradle/builditbigger/remote/FetchJokeTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/remote/FetchJokeTask.java
@@ -2,11 +2,14 @@ package com.udacity.gradle.builditbigger.remote;
 
 import android.os.AsyncTask;
 
-import java.io.IOException;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.jokeApi.JokeApi;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+
+import java.io.IOException;
 
 /**
  * Created by Singh on 05/01/18.
@@ -16,6 +19,7 @@ public class FetchJokeTask extends AsyncTask<String, Integer, String> {
 
     private static final String _ERROR = "ERROR";
     private FetchJokeTaskListener mListener;
+    private static JokeApi apiService = null;
 
     public FetchJokeTask(FetchJokeTaskListener listener) {
         mListener = listener;
@@ -37,23 +41,39 @@ public class FetchJokeTask extends AsyncTask<String, Integer, String> {
 
     @Override
     protected String doInBackground(String... strings) {
-        OkHttpClient client = new OkHttpClient();
-        Request request =
-                new Request.Builder()
-                        .url(mListener.getApi())
-                        .build();
-        try {
-            Response response = client.newCall(request).execute();
-            if (response.isSuccessful()) {
-                return response.body().string();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String response = getJokeFromLocal();
+        if (response != null) return response;
 
         return _ERROR;
     }
 
+    private String getJokeFromLocal() {
+        try {
+            return getService().joke().execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    private JokeApi getService() {
+        if (apiService == null) {  // Only do this once
+            JokeApi.Builder builder = new JokeApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl(mListener.getApi())
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            apiService = builder.build();
+        }
+
+        return apiService;
+    }
 
     @Override
     protected void onPostExecute(String joke) {
--- lib/src/main/java/com/udacity/gradle/builditbigger/lib/Joke.java
+++ lib/src/main/java/com/udacity/gradle/builditbigger/lib/Joke.java
@@ -3,6 +3,6 @@ package com.udacity.gradle.builditbigger.lib;
 public class Joke {
 
     public String getJoke() {
-        return "Dummy Joke";
+        return "Dummy Joke for Udacity.";
     }
 }
